7e758ba0e35348b1a7e4d51bbf042c9b7712afc0
thayna santana <thaynah@gmail.com>
2023-06-05 12:22:32 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,19 @@ public class Program {
 		System.out.println("Check-out date (dd/MM/yyyy)");
 		checkOut= sdf.parse(sc.next());
 		
+		
+		Date now = new Date(); // SOLUÇÃO MTO RUIM.
+		
+		if (checkIn.before(now) || checkOut.before(now)){
+			System.out.println("Error in reservation: Reservation dates for update must be future dates");
+		}
+		else if (!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+
+			
+		}
+		else {
+		
 		reservation.updateDates(checkIn, checkOut);
 		
 		System.out.println("Reservation:  " + reservation);
@@ -52,6 +65,5 @@ public class Program {
 		
 		
 		
-	}
-
-}
+	} }}
+	
